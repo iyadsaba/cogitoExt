f63ce08568e3ab237b8eb7f4af233b6187532bb0
iyad saba <iyadsaba@gmail.com>
2019-12-24 02:26:36 +0200

    CORB security issue

--- scripts/app.js
+++ scripts/app.js
@@ -10,20 +10,42 @@ function docReady(fn) {
     }
 }
 
+function setTotal(info){
+    document.getElementById("total").innerText = info.total;
+    console.log("%c setTotal invoked!","font-size:30px;")
+}
+
 docReady(function () {
     console.log("%c document ready ", "font-size:20px;color:yellow;")
     let showBtn = document.getElementById("show");
     let hideBtn = document.getElementById("hide");
+    let checkBtn = document.getElementById("check");
+    let countBtn = document.getElementById("count");
 
     hideBtn.addEventListener('click', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { todo: "hide" });
         });
     });
-
     showBtn.addEventListener('click', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { todo: "show" });
+            chrome.tabs.sendMessage(tabs[0].id, { todo: "show" } , setTotal);
+        });
+    });
+    checkBtn.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { todo: "notTreaded" });
+        });
+    });
+    countBtn.addEventListener('click', function () {
+        console.log("%c click event invoked!","font-size:30px;")
+        let hour =  document.getElementById("hour").value;
+        let minute =  document.getElementById("minute").value;
+        console.log(`hour : ${hour}`, "font-size:30px;")
+        console.log(`minute : ${minute}`, "font-size:30px;")
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { todo: "count" , hour , minute });
         });
     });
 
--- scripts/content.js
+++ scripts/content.js
@@ -1,61 +1,86 @@
-function showAllLeauges(){
-    console.log("%c showAllLeauges invoked ","font-size:20px;")
+function showAllLeauges() {
+    console.log("%c showAllLeauges invoked ", "font-size:20px;")
 
-    let leagues  = [...document.querySelectorAll("li.slide")];
-    leagues.forEach(function(el){
-            el.setAttribute("style","display:block;");
+    let leagues = [...document.querySelectorAll("li.slide")];
+    leagues.forEach(function (el) {
+        el.setAttribute("style", "display:block;");
     })
- }
- function showTradedLeauges(){
-    console.log("%c showTradedLeauges invoked ","font-size:20px;")
+}
+function showTradedLeauges() {
+    console.log("%c showTradedLeauges invoked ", "font-size:20px;")
 
-    const chosenLeagues = ['spain2' , 'germany3' , 'england2' , 'netherlands2','france2']; 
-    let leagues  = [...document.querySelectorAll(".match-league")];
-    leagues.forEach(function(el){
-        if(chosenLeagues.indexOf(el.getAttribute("title")) < 0){
-            el.closest("li").setAttribute("style","display:none;");
+    const chosenLeagues = ['spain2', 'germany3', 'england2', 'netherlands2', 'france2'];
+    let leagues = [...document.querySelectorAll(".match-league")];
+    leagues.forEach(function (el) {
+        if (chosenLeagues.indexOf(el.getAttribute("title")) < 0) {
+            el.closest("li").setAttribute("style", "display:none;");
         }
     });
- }
- function countMatches(hour,min){
-    let matches = [...document.querySelectorAll("li.slide")];
-    let total = 0;
-    matches.forEach(function(match){
-        if(isVisible(match)){
-            let matchKickOff = match.querySelector('.match-start-time').innerText.split(" ")[1].split(":");
+}
+function countMatches(hour, min) {
+    let matches = [...document.querySelectorAll("hmd-match-list-widget li.slide")].filter(match => !!isVisible(match));
+    return  matches.splice(getIndex(matches)).length;
+    function getIndex(arr,el){
+        arr.forEach(function (match , i) {
+            console.log(match)
+            let timeEl = match.querySelector(".match-start-time");
+            let matchKickOff = timeEl &&  timeEl.innerText && timeEl.innerText.split(" ")[1].split(":");
             let matchHour = matchKickOff[0];
             let matchMin = matchKickOff[1];
-            if(matchHour >= hour && min > matchMin){
-                ++total;
+            if (matchHour >= hour && min > matchMin) {
+                return i;
             } 
-        }
-    });
-    console.log(`%c total :${total}`, "font-size:20px;color:yellow");
-
-
-    function isVisible (ele) {
-        var style = window.getComputedStyle(ele);
-        return  style.width !== "0" &&
+        });
+    }
+}
+function isVisible(ele) {
+    var style = window.getComputedStyle(ele);
+    return style.width !== "0" &&
         style.height !== "0" &&
         style.opacity !== "0" &&
-        style.display!=='none' &&
-        style.visibility!== 'hidden';
-    }
-    
- }
+        style.display !== 'none' &&
+        style.visibility !== 'hidden';
+}
 
- console.log("%c content script file loaded", "font-size:30px;color:green;")
+console.log("%c content script file loaded", "font-size:30px;color:green;")
+chrome.runtime.sendMessage({ todo: "showPageAction" });
 
-chrome.runtime.sendMessage({todo: "showPageAction"});
-
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if (request.todo == "hide"){
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.todo == "hide") {
         console.log("%c hide action should trigger", "font-size:30px;color:green;")
-       showTradedLeauges();
+        showTradedLeauges();
     }
-    else if(request.todo == "show"){
+    else if (request.todo == "show") {
         console.log("%c show action should trigger", "font-size:30px;color:green;")
         showAllLeauges();
     }
+    else if (request.todo == 'notTreaded') {
+        let ended = [...document.querySelectorAll("time")].filter(match => isVisible(match));
+        let optimized = ended.filter((match) => !!match.getAttribute("datetime"));
+        let notOptimized = ended.filter((match) => !match.getAttribute("datetime"));
+        console.log(`%c optimized : ${optimized.length + 1}`, "font-size:30px;")
+        alert(` optimized : ${optimized.length + 1}`)
+        alert(` notOptimized : ${notOptimized.length + 1}`)
+
+        console.log(`%c notOptimized : ${notOptimized.length + 1}`, "font-size:30px;")
+    }
+    else if(request.todo == "count"){
+        console.log(`count event ocurred `, "font-size:30px; color:bule;")
+        console.log(`hour : ${request.todo.hour}`, "font-size:30px;;")
+        console.log(`minute : ${request.todo.minute}`, "font-size:30px;")
+        if(hour && minute){
+            console.log(`total : ${countMatches(request.todo.hour,request.todo.minute)}`, "font-size:30px; color:bule;")
+
+            try {
+                sendResponse( `{total :${countMatches(request.todo.hour,request.todo.minute)}}`);
+
+            } catch (error) {
+                sendResponse('{}');
+
+            }
+            // sendResponse( `{total :${countMatches(hour,minute)}}`);
+        }
+
+    }
+    sendResponse('{}');
 });
